1a2d1c28bb4e5e45e2f2bd33fee849233dbb1fb6
Surena05 <76176167+Surena05@users.noreply.github.com>
2020-12-17 12:03:04 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import openpyxl
+import pandas as pd
+
+df1 =openpyxl.load_workbook (r'C:\Python27\Test folder\test.xlsx')
+df =openpyxl.load_workbook (r'C:\Python27\Test folder\test1.xlsx')
+sheet_df=df.active
+sheet_df1 = df1.active
+maxRow = sheet_df.max_row
+maxCol = sheet_df.max_column
+for row in range (0,maxRow):
+    cell_df=sheet_df.cell(row+1,column=1)
+    cell_df1=sheet_df1.cell(row+1,column=1)
+    if cell_df==cell_df1:
+        for col in range(0,maxCol)
+            row+1
+        print(cell_df.value)
